1ac2ffcda26bdaa336cc62ae15057325f6245354
Josep Marcello <jspmarcello@live.com>
2023-02-18 22:49:52 +0700

    feat: finish consumer and producer

--- consumer/src/main.rs
+++ consumer/src/main.rs
@@ -1,8 +1,55 @@
-use log::info;
+use std::process;
+use std::time::Duration;
+use log::{error, info};
+use paho_mqtt::{Client, ConnectOptionsBuilder};
 
 fn main() {
     ::std::env::set_var("RUST_LOG", "INFO");
     env_logger::init();
 
-    info!("Hello, world!");
+    let addr = "localhost";
+    let host = 1883_u16;
+    let client = Client::new(format!("tcp://{}:{}", addr, host));
+    let client = match client {
+        Ok(c) => c,
+        Err(e) => {
+            error!("Can't create a new client: {}", e);
+            process::exit(1);
+        }
+    };
+
+    let opts = ConnectOptionsBuilder::new()
+        .keep_alive_interval(Duration::from_secs(20))
+        .clean_session(true)
+        .finalize();
+    if let Err(e) = client.connect(opts) {
+        error!("Can't connect to broker: {}", e);
+        process::exit(1);
+    }
+
+    match client.subscribe("test", 0) {
+        Err(e) => {
+            error!("Can't subscribe to broker: {}", e);
+            process::exit(1);
+        },
+        Ok(response) => {
+            info!("{}", response.subscribe_response().unwrap_or(-1));
+        },
+    };
+
+    for msg in client.start_consuming() {
+        if let Some(msg) = msg {
+            info!("{}", msg);
+        } else if !client.is_connected() {
+            error!("Got empty message and got disconnected");
+            break;
+        }
+    }
+
+    client.stop_consuming();
+
+    if let Err(e) = client.disconnect(None) {
+        error!("Can't disconnect from broker: {}", e);
+        process::exit(1);
+    }
 }
--- producer/src/main.rs
+++ producer/src/main.rs
@@ -1,8 +1,44 @@
-use log::info;
+use std::process;
+use std::thread::sleep;
+use std::time::Duration;
+use log::error;
+use paho_mqtt::{Client, ConnectOptionsBuilder, Message};
 
 fn main() {
     ::std::env::set_var("RUST_LOG", "INFO");
     env_logger::init();
 
-    info!("Hello, world!");
+    let addr = "localhost";
+    let host = 1883_u16;
+    let client = Client::new(format!("tcp://{}:{}", addr, host));
+    let client = match client {
+        Ok(c) => c,
+        Err(e) => {
+            error!("Can't create a new client: {}", e);
+            process::exit(1);
+        }
+    };
+
+    let opts = ConnectOptionsBuilder::new()
+        .keep_alive_interval(Duration::from_secs(20))
+        .clean_session(true)
+        .finalize();
+    if let Err(e) = client.connect(opts) {
+        error!("Can't connect to broker: {}", e);
+        process::exit(1);
+    }
+
+    for i in 0.. {
+        let msg = Message::new("test", format!("Hi anjing {}", i),0 );
+        if let Err(e) = client.publish(msg) {
+            error!("Can't send message to broker: {}", e);
+            break;
+        }
+        sleep(Duration::from_millis(500))
+    }
+
+    if let Err(e) = client.disconnect(None) {
+        error!("Can't disconnect from broker: {}", e);
+        process::exit(1);
+    }
 }
